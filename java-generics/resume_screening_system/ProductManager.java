1777b8e0445d5f56c51c9ee30ea72cc17df44fe2
bhuvannanda0011 <bhuvannanda0011@gmail.com>
2025-03-25 13:28:51 +0530

    [Bhuvan] Add. resume screening system

---
+++ java-generics/resume_screening_system/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager extends JobRole {
+    public ProductManager(String candidateName, String skills) {
+        super(candidateName, skills);
+    }
+
+    @Override
+    public void screenResume() {
+        System.out.println(getCandidateName() + " (Product Manager) is screened with skills: " + getSkills());
+    }
+}
