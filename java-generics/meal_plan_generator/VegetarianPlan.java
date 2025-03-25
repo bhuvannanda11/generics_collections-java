b0563a10912500b280b418214279c12b7139cb4b
bhuvannanda0011 <bhuvannanda0011@gmail.com>
2025-03-25 13:25:22 +0530

    [Bhuvan] Add. meal plan generator

---
+++ java-generics/meal_plan_generator/VegetarianPlan.java
@@ -0,0 +1,24 @@
+public class VegetarianPlan implements MealPlan{
+    private String mealName;
+    private int calories;
+
+    public VegetarianPlan (String mealName, int calories) {
+        this.mealName = mealName;
+        this.calories = calories;
+    }
+
+    @Override
+    public String getMealName() {
+        return mealName;
+    }
+
+    @Override
+    public int getCalories() {
+        return calories;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Vegetarian Meal: " + mealName + " (" + calories + " cal)");
+    }
+}
